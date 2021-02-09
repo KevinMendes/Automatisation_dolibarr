b51dff2e677f8f4465af2a70b74df67f483d31e0
KevinMendes <mendes.kevin35@gmail.com>
2021-02-09 14:05:26 +0100

    globalification des logins/url

--- dolibarr_auto/Scripts/ACC_FONCT/AF-001-N/Script1612874966385.groovy
+++ dolibarr_auto/Scripts/ACC_FONCT/AF-001-N/Script1612874966385.groovy
@@ -15,7 +15,8 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
-WebUI.callTestCase(findTestCase('Special/Connexion'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Special/Connexion'), [('Login') : GlobalVariable.Login, ('Password') : GlobalVariable.Password
+        , ('URL') : GlobalVariable.URL], FailureHandling.STOP_ON_FAILURE)
 
 WebUI.closeBrowser()
 
---
+++ dolibarr_auto/Test Suites/ACC_FONCT.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ACC_FONCT</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c7ad26c2-d7dc-49db-bb3e-57a57270a5a6</testSuiteGuid>
+   <testCaseLink>
+      <guid>fda3511b-7f5c-4d6e-8abe-425d1b0407d5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ACC_FONCT/AF-001-A1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7d6e3c4a-e410-4184-8190-ba52bc808938</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ACC_FONCT/AF-001-N</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
