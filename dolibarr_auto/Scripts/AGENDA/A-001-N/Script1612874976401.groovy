ec2d59a44e551071dc3c0c7333d4ad1b47fd51f2
HaoZancrow <bobon.julien@gmail.com>
2021-02-09 16:18:11 +0100

    Mon message de commit

--- dolibarr_auto/Scripts/AGENDA/A-001-N/Script1612874976401.groovy
+++ dolibarr_auto/Scripts/AGENDA/A-001-N/Script1612874976401.groovy
@@ -15,3 +15,5 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 
+WebUI.acceptAlert()
+
