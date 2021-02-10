2a69ef8dd2d5c6418b375fb11e08cd3925f69c5f
Helene-B17 <helene.beuchee@gmail.com>
2021-02-10 16:01:47 +0100

    T-001-A2-HELENE ajouté

---
+++ dolibarr_auto/Object Repository/T-001-A2-HELENE/Page_Tiers/span_April_fas fa-building paddingright cla_fed5f6.rs
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_April_fas fa-building paddingright cla_fed5f6</name>
+   <tag></tag>
+   <elementGuidId>1d21755b-164e-4193-aed3-37654dde77cd</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fas fa-building paddingright classfortooltip</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;id-right&quot;)/div[@class=&quot;fiche&quot;]/form[1]/div[@class=&quot;div-table-responsive&quot;]/table[@class=&quot;tagtable liste listwithfilterbefore&quot;]/tbody[1]/tr[@class=&quot;oddeven&quot;]/td[@class=&quot;tdoverflowmax200&quot;]/a[@class=&quot;classfortooltip refurl&quot;]/span[@class=&quot;fas fa-building paddingright classfortooltip&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='id-right']/div/form/div[2]/table/tbody/tr[3]/td/a/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//td/a/span</value>
+   </webElementXpaths>
+</WebElementEntity>
