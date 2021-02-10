3d209dcad9f26023da6ad0d7a07054c22fd41e3f
KevinMendes <mendes.kevin35@gmail.com>
2021-02-10 10:38:10 +0100

    setup-suite

---
+++ dolibarr_auto/Test Suites/SetUp_Suite.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SetUp_Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d0623e17-ca21-4928-b6c0-73a79ab60c52</testSuiteGuid>
+   <testCaseLink>
+      <guid>125bb44d-5464-48ca-82b1-875a4b608f6d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ACC_FONCT/AF-004-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0bdcb9f4-38aa-403d-bff7-018a96e90e40</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/ACC_FONCT/AF-003-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc0ea0a1-7119-4673-8bf9-a4bc136be73a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PROJET_SERVICE/PS-002-N</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>46f0ee4c-ea42-4b68-8217-3189282499be</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PROJET_SERVICE/PS-001-N</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
