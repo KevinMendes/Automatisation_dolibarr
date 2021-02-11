ee10001fb22c1957a9c015d5d5d7d7c6a7db98ee
Helene-B17 <helene.beuchee@gmail.com>
2021-02-11 10:38:23 +0100

    ajout exception AF-004-E

---
+++ dolibarr_auto/Object Repository/AF-004-N/Page_Configuration/span_SocitOrganisation_fas fa-exclamation-triangle pictowarning pictowarning.rs
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>span_SocitOrganisation_fas fa-exclamation-triangle pictowarning pictowarning</name>
+   <tag></tag>
+   <elementGuidId>3ffcea10-51a3-46b8-aeeb-de9ff3b0c67e</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>fas fa-exclamation-triangle pictowarning pictowarning</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Cliquez ici pour configurer les paramètres obligatoires</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;id-left&quot;)/div[@class=&quot;vmenu&quot;]/div[@class=&quot;blockvmenu blockvmenuimpair&quot;]/div[@class=&quot;menu_contenu menu_contenu_admin_company&quot;]/a[@class=&quot;vsmenu&quot;]/span[@class=&quot;fas fa-exclamation-triangle pictowarning pictowarning&quot;]</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='id-left']/div/div[3]/div[3]/a/span</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/a/span</value>
+   </webElementXpaths>
+</WebElementEntity>
